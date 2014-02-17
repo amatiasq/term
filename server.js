1ffa8154091aca01ac2d3702a23efdce27d0681d
A. Matías Quezada <amatiasq@gmail.com>
2014-02-17 19:38:13 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+//jshint node:true
+'use strict';
+
+var spawn = require('child_process').spawn;
+var express = require('express');
+var app = express();
+var server = app.listen(3001);
+var io = require('socket.io').listen(server);
+
+app.configure(function(){
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+console.log('Express server listening on port 3001');
+
+var special = {
+  'ENTER': '\n',
+  'TAB': '\t',
+  'BACKSPACE': '\b',
+  'ESCAPE': String.fromCharCode(27),
+  'FILE_END': String.fromCharCode(3),
+};
+
+io.sockets.on('connection', function (socket) {
+  console.log('[CONNECTED]');
+  socket.emit('info', { msg: 'The world is round, there is no up or down.' });
+
+  var shell = createShell();
+
+  socket.on('stdin', function(data) {
+    if (data.length > 1) {
+      if (special.hasOwnProperty(data))
+        data = special[data];
+      else
+        return console.log('cant write', data);
+    }
+
+    console.log('[STDIN]', data.charCodeAt(0), data);
+    shell.stdin.write(data);
+  });
+
+  shell.stdout.on('data', function(data) {
+    console.log('[STDOUT]', data);
+    socket.emit('stdout', data);
+  });
+  shell.stderr.on('data', function(data) {
+    console.log('[STDERR]', data);
+    socket.emit('stderr', data);
+  });
+  shell.on('close', function(code) {
+    console.log('[CLOSED]', code);
+    socket.emit('closed', code);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('[DISCONNECTED]');
+    shell.stdin.write('\nexit\n');
+  });
+});
+
+function createShell() {
+  var shell = spawn('bash', [ '-i' ]);
+  shell.stdout.setEncoding('utf8');
+  shell.stderr.setEncoding('utf8');
+  return shell;
+}
+

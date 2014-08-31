b99a6dca942c474b65713d72dddca1aef8aa96d8
A. Matías Quezada <amatiasq@gmail.com>
2014-08-31 18:56:24 +0200

    Added gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+//jshint maxlen:120, camelcase:false
+
+module.exports = function(grunt) {
+  'use strict';
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.initConfig({
+
+    files: {
+      all: 'web/**/*.js',
+      entry: 'web/main.js',
+      output: 'web/bundle.js',
+    },
+
+    browserify: {
+      build: {
+        options: {
+          transform: [ 'es6ify' ],
+          debug: true,
+        },
+        src: '<%= files.entry %>',
+        dest: '<%= files.output %>',
+      },
+    },
+
+    watch: {
+      js: {
+        files: '<%= files.all %>',
+        tasks: [ 'browserify:build' ],
+      },
+    }
+  });
+
+
+  grunt.registerTask('default', [ 'watch' ]);
+};

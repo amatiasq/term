0b2113870b53f5f6ff1b23d3c5004c1077e09189
A. Matías Quezada <amatiasq@gmail.com>
2021-04-16 21:08:37 +0200

    Looks like I tried a fresh start?

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-#!/usr/bin/env node
-//jshint node:true
-'use strict';
+const { createServer } = require("http");
+const telnet = require('telnet-client');
+const { Server } = require("ws");
 
-var fs = require('fs');
-var express = require('express');
-var app = require('./srv/app').app;
-var socket = require('./srv/socket');
-var errorhandler = require('errorhandler');
+const port = process.env.PORT || 17965;
+const server = createServer();
+const wss = new Server({ server });
 
-app.use(errorhandler());
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use('/web', express.static(__dirname + '/web'));
-app.use('*', function(req, res) {
-  fs.createReadStream('index.html').pipe(res);
+server.listen(port, () => console.log(`Websocket server ready at ${port}`));
+
+wss.on('connection', (ws, req) => {
+  const conn = new telnet();
+
+  ws.on('close', () => telnet.close());
+
+  ws.on('message', msg => {
+    const data = parseJson(msg);
+
+    if (data.type = 'CONNECT') {
+      conn.connect({
+
+      });
+    }
+  });
+
+  conn.on('connect', () => send({type: 'CONNECTED'}));
+
+  function send(data) {
+    ws.send(JSON.stringify(data));
+  }
 });
+
+function parseJson(text) {
+  try {
+    return JSON.parse(text);
+  } catch (e) {
+    console.warn('Invalid JSON:', text);
+    return INVALID_JSON;
+  }
+}
